b6dd952b65148cdf9c336c0cf61a31fe962df2b4
oheneralov <oheneralov@gmail.com>
2017-09-19 14:02:42 +0300

    Added logging

--- testing/functional/pom/src/test/java/com/webplane/webplane/AppTest.java
+++ testing/functional/pom/src/test/java/com/webplane/webplane/AppTest.java
@@ -2,6 +2,8 @@ package com.webplane.webplane;
 
 import java.util.concurrent.TimeUnit;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -25,9 +27,11 @@ public class AppTest extends TestCase {
 	HomePage homePage;
 	SimulatorPage simulatorPage;
 	TheoryPage theoryPage;
+	Logger logger;
 
 	public AppTest(String testName) {
 		super(testName);
+		
 	}
 
 	/**
@@ -38,14 +42,18 @@ public class AppTest extends TestCase {
 	}
 
 	public void setUp() throws Exception {
+		System.setProperty("log4j.configurationFile","C:\\alex\\training\\java\\planeonline\\testing\\functional\\pom\\configuration.xml");
+        logger = LogManager.getRootLogger();
 		System.setProperty("webdriver.gecko.driver", "C://alex//selenium//geckodriver-v0.18.0-win64//geckodriver.exe");
 		driver = new FirefoxDriver();
 		homePage = PageFactory.initElements(driver, HomePage.class);
 		simulatorPage = PageFactory.initElements(driver, SimulatorPage.class);
 		theoryPage = PageFactory.initElements(driver, TheoryPage.class);
+		logger.info("All classes initialized");
 	}
 
 	public void testMainPageLinks() {
+		logger.info("Testing main page links");
 		homePage.open(driver, Config.HomePage.baseURL);
 		assertEquals(Config.HomePage.baseURL, homePage.getCurrentUrl());
 		assertEquals("Home", homePage.getTitle());
@@ -56,6 +64,7 @@ public class AppTest extends TestCase {
 	}
 
 	public void testMainPageContent() {
+		logger.info("Testing main page content");
 		homePage.open(driver, Config.HomePage.baseURL);
 		assertEquals(Config.HomePage.content, homePage.getContent());
 	}
@@ -64,6 +73,7 @@ public class AppTest extends TestCase {
 	 * Open home page, click on simulator top link and check if no errors
 	 */
 	public void testSimulatorPageLinks() {
+		logger.info("Testing simulator page links");
 		simulatorPage.open(driver, Config.HomePage.baseURL);
 		// click on the simulator link
 		simulatorPage.getSimulatorTopLink().click();
@@ -73,6 +83,7 @@ public class AppTest extends TestCase {
 	}
 
 	public void testSimulatorPageContent() {
+		logger.info("Testing simulator page content");
 		simulatorPage.open(driver, Config.SimulatorPage.baseURL);
 		assertEquals(Config.SimulatorPage.content, simulatorPage.getContent());
 	}
@@ -81,6 +92,7 @@ public class AppTest extends TestCase {
 	 * Open home page, click on theory top link and check if no errors
 	 */
 	public void testTheoryPageLinks() {
+		logger.info("Testing theory page links");
 		theoryPage.open(driver, Config.HomePage.baseURL);
 		// click on the simulator link
 		theoryPage.getTheoryTopLink().click();
@@ -90,6 +102,7 @@ public class AppTest extends TestCase {
 	}
 
 	public void testTheoryPageContent() {
+		logger.info("Testing theory page content");
 		theoryPage.open(driver, Config.TheoryPage.baseURL);
 		assertEquals(Config.TheoryPage.content, theoryPage.getContent());
 	}
